3202bfd48abdcdf3bd40aa166dbe35de99baf25b
everctba <everson777@gmail.com>
2025-04-21 18:49:48 -0300

    printa matriz

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#define TAM_X 5
+#define TAM_Y 7
+
+#define VALOR_PADRAO 0
+#define VALOR_BOMBO 5
+
+int i = 0;
+int j = 0;
 
 int main()
 {
-    printf("Hello world!\n");
+    srand(time(NULL));
+    //%2d printa 2 casas
+
+    int campo_matriz[TAM_X][TAM_Y] = {VALOR_PADRAO};
+
+    for (i=0; i<TAM_X; i++){
+        for (j=0; j< TAM_Y; j++){
+           // printf("entrou\n");
+            printf("%d ",campo_matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+   // printf("Hello world!\n");
     return 0;
 }
