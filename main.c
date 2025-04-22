4fa8b816bd8dd1d95af8dbd3c7419bae4cf272c0
everctba <everson777@gmail.com>
2025-04-21 22:54:51 -0300

    atualizado lado maior direita concertado

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define TAM_2 8
 #define BASE 5
 #define VALOR_PADRAO 0
-#define VALOR_BOMBA 5
+#define VALOR_BOMBA 9
 
 //ctrl +shif + c Comenta
 //ctrl +shif + x descomenta
@@ -26,9 +26,9 @@ int main()
     //%2d printa 2 casas
 
     int campo_matriz[TAM_1][TAM_2] = {VALOR_PADRAO};
-    campo_matriz[2][5] = VALOR_BOMBA;
-//    campo_matriz[1][1] = VALOR_BOMBA;
-    campo_matriz[1][6] = VALOR_BOMBA;
+//    campo_matriz[2][5] = VALOR_BOMBA;
+    campo_matriz[2][7] = VALOR_BOMBA;
+//    campo_matriz[1][6] = VALOR_BOMBA;
 
     //TAMANHO TOTAL da MATRIZ
     tamanho_matriz = sizeof(campo_matriz);
@@ -37,8 +37,22 @@ int main()
     printf("Tamanho Matriz = %d  | ", total_elementos);
 
     //CALCULO DE BOMBAS
-    total_elementos = ceil(total_elementos *0.15);
+        total_elementos = ceil(total_elementos *0.15);
     printf("15 Porcento de bombas = %d  | ", total_elementos);
+
+
+    //ADICIONA BOMBAS ALEATORIAS NA MATRIZ
+//    int e = 1;
+//    int linha;
+//    int coluna;
+//    while (e<=total_elementos) {
+//       linha  = rand() % TAM_1;
+//       coluna = rand() % TAM_2;
+//        if (campo_matriz[linha][coluna] == VALOR_PADRAO){
+//        campo_matriz[linha][coluna] = VALOR_BOMBA;
+//        e = e+1;
+//        }
+//    }
 
     //identifica se é bomba
     for (i=0; i<TAM_1; i++)
@@ -53,15 +67,44 @@ int main()
                    for(p=-1;p<2;p++){
                     printf("p = %2d    ",p);
                     printf("k = %2d    ",k);
+                    printf("\n");
 //                    campo_matriz[i+k][j+p] = VALOR_PADRAO+1;
 //                    int valor_p = campo_matriz[i+k][j+p];
 //                    printf("Matriz + p = %d\n",valor_p);
                     //se o valor nao for da bomba (valor central)
-                    if (campo_matriz[i+k][j+p] != VALOR_BOMBA){
+                    //&& campo_matriz[i+k][j+p] >= 0
+//                    && (campo_matriz[i][j] + p < TAM_2  )
+//                    int valorx = campo_matriz[i+k][j+p];
+//                    printf("valorx = %2d\n",valorx);
+//                     printf("matriz ij = %2d\n",campo_matriz[i+k][j+p]);
+//                     printf("                    matriz ij = %2d\n",campo_matriz[i][j]);
+
+                        if (campo_matriz[i+k][j+p]!= VALOR_BOMBA ){
 //                      printf("valor i %d\n", i);
-//
-                        campo_matriz[i+k][j+p] = campo_matriz[i+k][j+p]+1;
+                            if (j+p >= TAM_2 ) {
+                                    //&& (i+k && j+p < 0 )
+                                 printf("ENTROU NA CONDICAO\n");
+                                continue;
+
+                            }
+                            else {
+                                campo_matriz[i+k][j+p] = campo_matriz[i+k][j+p]+1;
+                            }
+
+//                         if ((campo_matriz[i+k] >=0) && (campo_matriz[j+p] >= 0) && (campo_matriz[i+k] < TAM_1) && (campo_matriz[j+p] < TAM_2)){
+//                            campo_matriz[i+k][j+p] = campo_matriz[i+k][j+p]+1;
+//                         }
+                           // {}
+
+
+
                        }
+
+
+
+
+
+
 //                      printf("matriz ij = %d\n",campo_matriz[i+k][j+p]);
 //                      printf("matriz j %d\n",campo_matriz[j+p]);
 //
@@ -79,7 +122,7 @@ int main()
 
     printf("\n");
     //printa matriz
-    printf("     1 2 3 4 5 6 7 8\n");
+    printf("     0 1 2 3 4 5 6 7\n");
     printf("     ---------------\n");
     for (i=0; i<TAM_1; i++)
     {
